b3fbb0e8759455ab00fa46d23f175ba299ba16d3
Aaron Skinner <aaroncurtisskinner@gmail.com>
2021-09-22 16:19:52 -0400

    Add project files.

---
+++ shipCargoGame/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace shipCargoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship();
+
+            string welcomeMessage = "Welcome to Cargo Ship. The goal of this game is to load the ship to its max capacity by " +
+                "adding motor cycles, cars, turcks, and train cars. A cycle takes 3 units of weight. A car takes 5 units. A truck takes 11 units. A train car takes 17 units.";
+
+            Console.WriteLine(welcomeMessage);
+
+            // repeats until the player gets the ship loaded to capacity
+            while(ship.Capacity != ship.getShipLoad())
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("THe ship capacity is " + ship.Capacity);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("The ship has " + ship.cycleCount + " motor cycles on board");
+                Console.WriteLine("The ship has " + ship.carCount + " cars on board");
+                Console.WriteLine("The ship has " + ship.truckCount + " trucks on board");
+                Console.WriteLine("The ship has " + ship.trainCarCount + " train cars on board");
+                Console.WriteLine("The ship currently has " + ship.getShipLoad() + " total units on baord");
+
+                int x;
+                Console.WriteLine("How many motor cycles would you like on board the Ship");
+                x = int.Parse(Console.ReadLine());
+
+                ship.cycleCount = x;
+
+
+
+                Console.WriteLine("How many cars would you like on board the Ship");
+                x = int.Parse(Console.ReadLine());
+
+                ship.carCount = x;
+
+                Console.WriteLine("How many trucks would you like on board the Ship");
+                x = int.Parse(Console.ReadLine());
+
+                ship.truckCount = x;
+
+                Console.WriteLine("How many train cars would you like on board the Ship");
+                x = int.Parse(Console.ReadLine());
+
+                ship.trainCarCount = x;
+
+                Console.WriteLine("The ship now has " + ship.getShipLoad() + " total units of weight on board");
+
+                if(ship.overUnder() > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("The ship still has room to spare. Load more items");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The ship has been overloaded. Take some items off");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("the ship has been filled to capacity.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ shipCargoGame/Ship.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shipCargoGame
+{
+    class Ship
+    {
+        const int CYCLE_WEIGHT = 3;
+        const int CAR_WEIGHT = 5;
+        const int TRUCK_WEIGHT = 11;
+        const int TRAIN_WEIGHT = 17;
+
+        const int MAX_WEIGHT = 10;
+
+
+        public int Capacity { get; set; }
+        public int cycleCount { get; set; }
+        public int carCount { get; set; }
+        public int truckCount { get; set; }
+        public int trainCarCount { get; set; }
+
+        Random random = new Random();
+
+
+        // methods (functions)
+        public int getShipLoad()
+        {
+            // calculate the total weight of the cargo on the ship.
+
+            return cycleCount * CYCLE_WEIGHT+ carCount * CAR_WEIGHT + truckCount * TRUCK_WEIGHT + trainCarCount * TRAIN_WEIGHT;
+        }
+
+        // constructor - called when a new instance is created.
+        public Ship()
+        {
+            cycleCount = 0;
+            carCount = 0;
+            trainCarCount = 0;
+            truckCount = 0;
+
+            // create a random sized ship.
+            Capacity = random.Next(MAX_WEIGHT) * CYCLE_WEIGHT + random.Next(MAX_WEIGHT) * CAR_WEIGHT + random.Next(MAX_WEIGHT) * TRUCK_WEIGHT + random.Next(MAX_WEIGHT) * TRAIN_WEIGHT;
+            
+        }
+
+        public int overUnder()
+        {
+            // return a value of how loaded the ship is
+            return Capacity - getShipLoad();
+        }
+
+        public override string ToString()
+        {
+            return "Capacity = " + Capacity + ", CurrentLoad = " + getShipLoad();
+        }
+    }
+
+}
